4042d20bf600d4ccbadcbd8c8c2ece19aade09a6
kaatinga <kaatinga@gmail.com>
2020-02-25 12:15:45 +0300

    code

--- main.go
+++ main.go
@@ -1,1 +1,33 @@
 package assets
+
+import (
+	"strconv"
+)
+
+// StUint16 converts input string to uint16 type
+func StUint16(inputString string) (result uint16, ok bool) {
+
+	var err error // to store an error if it occurs
+	var tmpUint16 uint64 // a temporary uint64 value
+
+	tmpUint16, err = strconv.ParseUint(inputString, 10, 64)
+	result = uint16(tmpUint16)
+	if err == nil {
+		ok = true
+	}
+
+	return
+}
+
+// StBool converts input string to bool type
+func StBool(inputString string) (result bool, ok bool) {
+
+	var err error // to store an error if it occurs
+
+	result, err = strconv.ParseBool(inputString)
+	if err == nil {
+		ok = true
+	}
+
+	return
+}

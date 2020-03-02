0879834b40534646ca788f4efe1f8e51661e2322
kaatinga <kaatinga@gmail.com>
2020-03-02 15:49:57 +0300

    an additional check was added

--- main.go
+++ main.go
@@ -5,33 +5,31 @@ import (
 )
 
 // StUint16 converts input string to uint16 type
-func StUint16(inputString string) (result uint16, ok bool) {
+func StUint16(inputString string) (uint16, bool) {
+	var err error  // to store error result
+	var tmpInt int // a temporary int value
 
-	var err error // to store error result
-	var tmpUint16 uint64 // a temporary uint64 value
-
-	tmpUint16, err = strconv.ParseUint(inputString, 10, 64)
-	result = uint16(tmpUint16)
+	tmpInt, err = strconv.Atoi(inputString)
 	if err == nil {
-		ok = true
+		if tmpInt >= 0 && tmpInt < 65536 {
+			return uint16(tmpInt), true
+		}
 	}
-
-	return
+	return 0, false
 }
 
 // StByte converts input string to Byte type
-func StByte(inputString string) (result byte, ok bool) {
+func StByte(inputString string) (byte, bool) {
+	var err error  // to store error result
+	var tmpInt int // a temporary int value
 
-	var err error       // to store error result
-	var tmpInt64 int // a temporary uint64 value
-
-	tmpInt64, err = strconv.Atoi(inputString)
+	tmpInt, err = strconv.Atoi(inputString)
 	if err == nil {
-		ok = true
-		result = byte(tmpInt64)
+		if tmpInt >= 0 && tmpInt < 256 {
+			return byte(tmpInt), true
+		}
 	}
-
-	return
+	return 0, false
 }
 
 // StBool converts input string "true" to bool type

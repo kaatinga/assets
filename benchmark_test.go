3d59cb8476148e3c82c5f6d0c3962a62c348d403
Kaatinga <painter@3lines.club>
2021-04-08 17:45:43 +0300

    benchmarks updated

--- benchmark_test.go
+++ benchmark_test.go
@@ -5,30 +5,49 @@ import (
 	"testing"
 )
 
-func BenchmarkUint162Bytes(b *testing.B) {
+//func BenchmarkUint162Bytes(b *testing.B) {
+//
+//	b.ReportAllocs()
+//	for i := 0; i < b.N; i++ {
+//		Uint162Bytes(199)
+//		Uint162Bytes(0)
+//		Uint162Bytes(55)
+//	}
+//}
+//
+//func BenchmarkByte2Bytes(b *testing.B) {
+//
+//	b.ReportAllocs()
+//	for i := 0; i < b.N; i++ {
+//		Byte2Bytes(199)
+//		Byte2Bytes(0)
+//		Byte2Bytes(55)
+//	}
+//}
 
+//func BenchmarkByte2String(b *testing.B) {
+//
+//	b.ReportAllocs()
+//	for i := 0; i < b.N; i++ {
+//		Byte2String(199)
+//		Byte2String(0)
+//		Byte2String(55)
+//	}
+//}
+
+func BenchmarkUint162String(b *testing.B) {
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
-		Uint162Bytes(199)
-		Uint162Bytes(0)
-		Uint162Bytes(55)
-	}
-}
-
-func BenchmarkByte2Bytes(b *testing.B) {
-
-	b.ReportAllocs()
-	for i := 0; i < b.N; i++ {
-		Byte2Bytes(199)
-		Byte2Bytes(0)
-		Byte2Bytes(55)
+		Uint162String(19999)
+		Uint162String(0)
+		Uint162String(55)
 	}
 }
 
 func BenchmarkStrvconvItoa(b *testing.B) {
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
-		strconv.Itoa(199)
+		strconv.Itoa(19999)
 		strconv.Itoa(0)
 		strconv.Itoa(55)
 	}

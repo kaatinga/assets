2d77bef03075bdfdf126ada50988de7c34a3a785
Kaatinga <painter@3lines.club>
2021-04-09 13:15:16 +0300

    unit test for Uint16String composed

--- main_test.go
+++ main_test.go
@@ -548,6 +548,32 @@ func TestUint162Bytes(t *testing.T) {
 	}
 }
 
+func TestUint162String(t *testing.T) {
+
+	tests := []struct {
+		result string
+		num    uint16
+	}{
+		{"199", 199},
+		{"1999", 1999},
+		{"222", 222},
+		{"1", 1},
+		{"0", 0},
+		{"55555", 55555},
+		{"12345", 12345},
+		{"10000", 10000},
+		{"9999", 9999},
+	}
+	for _, tt := range tests {
+		t.Run(tt.result, func(t *testing.T) {
+			got := Uint162String(tt.num)
+			if got != tt.result {
+				t.Errorf("Uint162String() = %v, want %v", got, tt.result)
+			}
+		})
+	}
+}
+
 func TestByte2Bytes(t *testing.T) {
 
 	tests := []struct {
@@ -592,7 +618,7 @@ func TestByte2String(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.result, func(t *testing.T) {
 			if got := Byte2String(tt.num); !reflect.DeepEqual(got, tt.result) {
-				t.Errorf("Byte2Bytes() = %v, want %v", got, tt.result)
+				t.Errorf("Byte2String() = %v, want %v", got, tt.result)
 			}
 		})
 	}

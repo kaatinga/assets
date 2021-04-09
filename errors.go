c7bd9fd816ff0d5f513927332294c7fe51b3781f
Kaatinga <painter@3lines.club>
2021-04-09 17:23:42 +0300

    errors for String2Uint32() were added

--- errors.go
+++ errors.go
@@ -1,1 +1,22 @@
 package assets
+
+// IntError - Error type based on int value.
+type IntError int
+
+// Error returns error description.
+func (err IntError) Error() string {
+	return errorDescriptions[err]
+}
+
+// errorDescriptions contains descriptions for the IntError errors.
+var errorDescriptions = []string{
+	0: "the input string is not a number",
+	1: "the input string is a number, but the value exceeds the maximum value",
+}
+
+const (
+
+	// String2Uint32 errors:
+	ErrNotUint32 IntError = iota
+	ErrNumberExceedMaxUint32Value
+)

e7c969f0f2ce2317f51b0f46bd47ef4d133af012
Kaatinga <painter@3lines.club>
2021-04-09 17:27:10 +0300

    String2Uint32 was implemented

--- strconv.go
+++ strconv.go
@@ -1,1 +1,33 @@
 package assets
+
+const UnicodeMask = 0xf
+
+// String2Uint32 checks and converts input string to uint32 type.
+func String2Uint32(input string) (output uint32, err error) {
+
+	if len(input) == 0 {
+		return 0, ErrNotUint32
+	}
+
+	var i int
+	for {
+
+		if i == len(input) {
+			break
+		}
+
+		if input[i] < 0x30 || input[i] > 0x39 {
+			return 0, ErrNotUint32
+		}
+
+		output = (output << 3) + (output << 1) + uint32(input[i])&UnicodeMask
+
+		if output == 0 && i > 8 {
+			return 0, ErrNumberExceedMaxUint32Value
+		}
+
+		i++
+	}
+
+	return
+}

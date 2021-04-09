8f8ba650c7c5b3b2171154115b269673cbb9b501
Kaatinga <painter@3lines.club>
2021-04-09 17:23:11 +0300

    SuperBytesToUint32() was moved to deprecated.go file

--- deprecated.go
+++ deprecated.go
@@ -1,1 +1,44 @@
 package assets
+
+// Deprecated: SuperBytesToUint32 checks and converts input string to uint32 type
+func SuperBytesToUint32(input string) (output uint32, ok bool) {
+
+	var output64 uint64
+
+	if input[0] < 48 || input[0] > 57 {
+		return
+	}
+
+	var (
+		value rune
+		key   int
+	)
+
+MainLoop:
+	for key, value = range input {
+
+		switch key {
+		case 0:
+			// processing the first byte
+			output64 = uint64(value) - 48
+			continue MainLoop
+		default:
+			if value < 48 || value > 57 {
+				ok = true
+				break MainLoop
+			}
+		}
+
+		// multiply to 10 and sum
+		output64 = output64*10 + uint64(value) - 48
+
+		if output64 >= maxUint32 {
+			// the number exceeds the uint32 limit!
+			return 0, false
+		}
+	}
+
+	output = uint32(output64)
+	ok = true
+	return
+}

4613ea574bbebebf5770a68186fe5fc2e7147275
Kaatinga <painter@3lines.club>
2021-04-09 17:26:21 +0300

    benchmarks for String2Uint32 was added

--- benchmark_test.go
+++ benchmark_test.go
@@ -35,20 +35,38 @@ import (
 //	}
 //}
 
-func BenchmarkUint162String(b *testing.B) {
+//func BenchmarkUint162String(b *testing.B) {
+//	b.ReportAllocs()
+//	for i := 0; i < b.N; i++ {
+//		Uint162String(19999)
+//		Uint162String(0)
+//		Uint162String(55)
+//	}
+//}
+//
+//func BenchmarkStrvconvItoa(b *testing.B) {
+//	b.ReportAllocs()
+//	for i := 0; i < b.N; i++ {
+//		strconv.Itoa(19999)
+//		strconv.Itoa(0)
+//		strconv.Itoa(55)
+//	}
+//}
+
+func BenchmarkString2Uint32(b *testing.B) {
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
-		Uint162String(19999)
-		Uint162String(0)
-		Uint162String(55)
+		String2Uint32("0")
+		String2Uint32("012")
+		String2Uint32("66555")
 	}
 }
 
-func BenchmarkStrvconvItoa(b *testing.B) {
+func BenchmarkStrvconv_Atoi(b *testing.B) {
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
-		strconv.Itoa(19999)
-		strconv.Itoa(0)
-		strconv.Itoa(55)
+		strconv.Atoi("0")
+		strconv.Atoi("012")
+		strconv.Atoi("66555")
 	}
 }

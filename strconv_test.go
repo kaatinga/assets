20c60578bb16868a89cae0c8cc5473003fe362a9
Kaatinga <painter@3lines.club>
2021-04-09 17:27:30 +0300

    init test for String2Uint32 was added

--- strconv_test.go
+++ strconv_test.go
@@ -1,1 +1,36 @@
 package assets
+
+import "testing"
+
+func TestString2Uint32(t *testing.T) {
+
+	tests := []struct {
+		name       string
+		input      string
+		wantOutput uint32
+		wantErr    error
+	}{
+		{"correct string 42", "42", 42, nil},
+		{"correct string 12", "12", 12, nil},
+		{"correct input 012", "012", 12, nil},
+		{"correct input 0)", "0", 0, nil},
+		{"incorrect input []byte{13, 10}", string([]byte{13, 10}), 0, ErrNotUint32},
+		{"incorrect input []byte{0}", string([]byte{0}), 0, ErrNotUint32},
+		{"incorrect string 4294967296", "4294967296", 0, ErrNumberExceedMaxUint32Value},
+		{"incorrect string 429496729612", "429496729612", 0, ErrNumberExceedMaxUint32Value},
+		{"incorrect string -1", "-1", 0, ErrNotUint32},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotOutput, err := String2Uint32(tt.input)
+			if err != tt.wantErr {
+				t.Errorf("String2Uint32() error = %v, wantErr %v", err, tt.wantErr)
+				t.Log(gotOutput)
+				return
+			}
+			if gotOutput != tt.wantOutput {
+				t.Errorf("String2Uint32() gotOutput = %v, want %v", gotOutput, tt.wantOutput)
+			}
+		})
+	}
+}

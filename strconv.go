5290e0bff23e22eedbdb434d5aa1f8ac44a90022
Kaatinga <painter@3lines.club>
2021-04-10 14:21:39 +0300

    String2Byte() is implemented, String2Uint32() and Bytes2Uint32() are fixed for some cases

--- strconv.go
+++ strconv.go
@@ -1,12 +1,20 @@
 package assets
 
-const UnicodeMask = 0xf
+const (
+	UnicodeMaskUint32 uint32 = 0xf
+	UnicodeMaskByte   byte   = 0xf
+	ByteLengthMask    int    = 0x3
+)
 
 // String2Uint32 checks and converts input string to uint32 type.
 func String2Uint32(input string) (output uint32, err error) {
 
 	if len(input) == 0 {
 		return 0, ErrNotUint32
+	}
+
+	if len(input) > 10 {
+		return 0, ErrNumberExceedMaxUint32Value
 	}
 
 	var i int
@@ -20,7 +28,7 @@ func String2Uint32(input string) (output uint32, err error) {
 			return 0, ErrNotUint32
 		}
 
-		output = (output << 3) + (output << 1) + uint32(input[i])&UnicodeMask
+		output = (output << 3) + (output << 1) + uint32(input[i])&UnicodeMaskUint32
 
 		if output == 0 && i > 8 {
 			return 0, ErrNumberExceedMaxUint32Value
@@ -39,6 +47,10 @@ func Bytes2Uint32(input []byte) (output uint32, err error) {
 		return 0, ErrNotUint32
 	}
 
+	if len(input) > 10 {
+		return 0, ErrNumberExceedMaxUint32Value
+	}
+
 	var i int
 	for {
 
@@ -50,7 +62,7 @@ func Bytes2Uint32(input []byte) (output uint32, err error) {
 			return 0, ErrNotUint32
 		}
 
-		output = (output << 3) + (output << 1) + uint32(input[i])&UnicodeMask
+		output = (output << 3) + (output << 1) + uint32(input[i])&UnicodeMaskUint32
 
 		if output == 0 && i > 8 {
 			return 0, ErrNumberExceedMaxUint32Value
@@ -60,4 +72,35 @@ func Bytes2Uint32(input []byte) (output uint32, err error) {
 	}
 
 	return
+}
+
+// String2Byte checks and converts input string to uint32 type.
+func String2Byte(input string) (output byte, err error) {
+
+	length := len(input)
+	if length&^ByteLengthMask != 0 {
+		return 0, ErrNotByte
+	}
+
+	var i int
+	for {
+
+		if i == length {
+			break
+		}
+
+		if input[i] < 0x30 || input[i] > 0x39 {
+			return 0, ErrNotByte
+		}
+
+		output = (output << 3) + (output << 1) + input[i]&UnicodeMaskByte
+
+		if output == 0 && i > 1 {
+			return 0, ErrNumberExceedMaxByteValue
+		}
+
+		i++
+	}
+
+	return output, nil
 }

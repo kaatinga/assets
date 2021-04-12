20abb15b44ac536f19cfb9196e977d3618338d2f
Kaatinga <painter@3lines.club>
2021-04-12 13:40:54 +0300

    the benchmarks are now fair

--- benchmark_test.go
+++ benchmark_test.go
@@ -57,8 +57,8 @@ func BenchmarkString2Byte(b *testing.B) {
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
 		String2Byte("0")
-		String2Byte("12")
-		String2Byte("66555")
+		String2Byte("255")
+		String2Byte("25549672951")
 	}
 }
 
@@ -66,8 +66,8 @@ func BenchmarkString2Uint32(b *testing.B) {
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
 		String2Uint32("0")
-		String2Uint32("12")
-		String2Uint32("66555")
+		String2Uint32("255")
+		String2Uint32("25549672951")
 	}
 }
 
@@ -75,25 +75,25 @@ func BenchmarkStrvconv_Atoi(b *testing.B) {
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
 		strconv.Atoi("0")
-		strconv.Atoi("12")
-		strconv.Atoi("66555")
+		strconv.Atoi("255")
+		strconv.Atoi("25549672951")
 	}
 }
 
-func BenchmarkBytes2Uint32(b *testing.B) {
-	b.ReportAllocs()
-	for i := 0; i < b.N; i++ {
-		Bytes2Uint32([]byte{0})
-		Bytes2Uint32([]byte{0x31, 0x32})
-		Bytes2Uint32([]byte{0x36, 0x36, 0x35, 0x35, 0x35})
-	}
-}
+//func BenchmarkBytes2Uint32(b *testing.B) {
+//	b.ReportAllocs()
+//	for i := 0; i < b.N; i++ {
+//		Bytes2Uint32([]byte{0})
+//		Bytes2Uint32([]byte{0x32, 0x35, 0x35})
+//		Bytes2Uint32([]byte{0x32, 0x35, 0x35, 0x34, 0x39, 0x36, 0x37, 0x32, 0x39, 0x35, 0x31})
+//	}
+//}
 
-func BenchmarkStrvconv_AtoiPlusBytesConversion(b *testing.B) {
-	b.ReportAllocs()
-	for i := 0; i < b.N; i++ {
-		strconv.Atoi(string([]byte{0}))
-		strconv.Atoi(string([]byte{0x31, 0x32}))
-		strconv.Atoi(string([]byte{0x30, 0x31, 0x32}))
-	}
-}
+//func BenchmarkStrvconv_AtoiPlusBytesConversion(b *testing.B) {
+//	b.ReportAllocs()
+//	for i := 0; i < b.N; i++ {
+//		strconv.Atoi(string([]byte{0}))
+//		strconv.Atoi(string([]byte{0x32, 0x35, 0x35})
+//		//strconv.Atoi(string([]byte{0x36, 0x36, 0x35, 0x35, 0x35}))
+//	}
+//}

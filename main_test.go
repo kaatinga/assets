7400516e3695d0e984193879ba08a523bf4ab6c1
Kaatinga <painter@3lines.club>
2021-05-09 16:38:17 +0300

    delete redundant tests

--- main_test.go
+++ main_test.go
@@ -522,32 +522,6 @@ func TestIsEmailValid(t *testing.T) {
 	}
 }
 
-func TestUint162Bytes(t *testing.T) {
-
-	tests := []struct {
-		result string
-		num    uint16
-	}{
-		{"199", 199},
-		{"1999", 1999},
-		{"222", 222},
-		{"1", 1},
-		{"0", 0},
-		{"55555", 55555},
-		{"12345", 12345},
-		{"10000", 10000},
-		{"9999", 9999},
-	}
-	for _, tt := range tests {
-		t.Run(tt.result, func(t *testing.T) {
-			got := Uint162Bytes(tt.num)
-			if !reflect.DeepEqual(got, []byte(tt.result)) {
-				t.Errorf("Uint162Bytes() = %v, want %v", string(got), tt.result)
-			}
-		})
-	}
-}
-
 func TestUint162String(t *testing.T) {
 
 	tests := []struct {
@@ -569,31 +543,6 @@ func TestUint162String(t *testing.T) {
 			got := Uint162String(tt.num)
 			if got != tt.result {
 				t.Errorf("Uint162String() = %v, want %v", got, tt.result)
-			}
-		})
-	}
-}
-
-func TestByte2Bytes(t *testing.T) {
-
-	tests := []struct {
-		result string
-		num    byte
-	}{
-		{"199", 199},
-		{"99", 99},
-		{"100", 100},
-		{"255", 255},
-		{"222", 222},
-		{"0", 0},
-		{"9", 9},
-		{"10", 10},
-		{"1", 1},
-	}
-	for _, tt := range tests {
-		t.Run(tt.result, func(t *testing.T) {
-			if got := Byte2Bytes(tt.num); !reflect.DeepEqual(got, []byte(tt.result)) {
-				t.Errorf("Byte2Bytes() = %v, want %v", got, tt.result)
 			}
 		})
 	}

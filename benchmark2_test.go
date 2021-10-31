a8ff31e1fd546db8300307b321c6d5d4b9937c74
Michael Gunkoff <painter@3lines.club>
2021-10-31 20:54:30 +0300

    new package files

---
+++ benchmark2_test.go
@@ -0,0 +1,21 @@
+package assets
+
+import (
+	"testing"
+)
+
+func BenchmarkGenPassword(b *testing.B) {
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		GenPassword(40)
+	}
+}
+
+func BenchmarkGenPasswordAsBytes(b *testing.B) {
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		GenPasswordAsBytes(40)
+	}
+}
---
+++ gen_password_test.go
@@ -0,0 +1,58 @@
+package assets
+
+import (
+	"testing"
+)
+
+func TestGenPassword(t *testing.T) {
+
+	tests := []struct {
+		name      string
+		length    byte
+		strLength int
+		wantErr   bool
+	}{
+		{"Zero value", 0, 7, false},
+		{"6", 6, 6, false},
+		{"16", 16, 16, false},
+	}
+
+	// nolint:scopelint
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotStr, err := GenPassword(tt.length)
+			ok := err != nil
+			if ok != tt.wantErr {
+				t.Errorf("GenPassword() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if len(gotStr) != tt.strLength {
+				t.Errorf("GenPassword() gotStr = %v, want %v", gotStr, tt.strLength)
+			}
+		})
+	}
+}
+
+func TestGenPasswordAsBytes(t *testing.T) {
+
+	tests := []struct {
+		name      string
+		length    byte
+		strLength int
+	}{
+		{"Zero value", 0, 7},
+		{"6", 6, 6},
+		{"16", 16, 16},
+	}
+
+	// nolint:scopelint
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotStr := GenPasswordAsBytes(tt.length)
+			t.Log(string(gotStr))
+			if len(gotStr) != tt.strLength {
+				t.Errorf("GenPasswordAsBytes() got length = %v, want length %v", len(gotStr), tt.strLength)
+			}
+		})
+	}
+}

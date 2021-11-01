e73d62aecd879c6289458e432ab6bf51c13270e6
Michael Gunkoff <painter@3lines.club>
2021-11-01 10:03:49 +0300

    GenPassword() implementation was substituted with a faster one

--- gen_password.go
+++ gen_password.go
@@ -2,7 +2,6 @@ package assets
 
 import (
 	"math/rand"
-	"strings"
 )
 
 const (
@@ -11,25 +10,8 @@ const (
 )
 
 // GenPassword generates a password of a set length.
-func GenPassword(length byte) (string, error) {
-
-	if length == 0 { // in case we do not want to point out the length we can set zero
-		length = 7
-	}
-
-	var (
-		builder strings.Builder
-		err     error
-	)
-
-	for i := byte(0); i < length; i++ {
-		err = builder.WriteByte(chars[rand.Intn(charsLength)]) //nolint:gosec
-		if err != nil {
-			return "", err
-		}
-	}
-
-	return builder.String(), nil
+func GenPassword(length byte) string {
+	return string(GenPasswordAsBytes(length))
 }
 
 // GenPasswordAsBytes generates a password of a set length.
